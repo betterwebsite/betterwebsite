8c057bc2c428fb3787550dbdbfbbe2d45239b517
Jordan Babb <normanwebdesigners@gmail.com>
2025-02-05 12:19:13 -0600

    Big Changes
    -Jordan

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,21 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Better Website",
+    "email": "normanwebdesigners@gmail.com",
+    "phoneForTel": "859-462-8431",
+    "phoneFormatted": "(859) 462-8431",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
+        "lineOne": "",
+        "lineTwo": "",
+        "city": "Tulsa",
+        "state": "OK",
+        "zip": "",
         "country": "US",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
+        "mapLink": ""
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.betterwebsite.ai"
 };
